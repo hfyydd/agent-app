1511d5bed3a4f655bf2e5d10214462a8acdf8936
hfyydd <85127658@qq.com>
2024-08-20 13:20:19 +0800

    加了对workflows表的订阅 实现实时更新浏览量

--- components/TotalViews.tsx
+++ components/TotalViews.tsx
@@ -1,27 +1,43 @@
 "use client"
+import { createClient } from '@/utils/supabase/client';
 import { useEffect, useState } from 'react';
 
 export default function TotalViews() {
-  const [totalViews, setTotalViews] = useState<number | null>(null);
+    const supabase = createClient();
+    const [totalViews, setTotalViews] = useState<number | null>(null);
 
-  useEffect(() => {
+    useEffect(() => {
+        fetchTotalViews();
+
+        const channel = supabase
+            .channel('table-db-changes')
+            .on('postgres_changes',
+                { event: '*', schema: 'public', table: 'workflows' },
+                (payload) => {
+                    fetchTotalViews();
+                }
+            )
+            .subscribe((status) => {
+            });
+
+        return () => {
+            supabase.removeChannel(channel);
+        };
+    }, []);
+
     async function fetchTotalViews() {
-      try {
-        const response = await fetch('/api/views');
-        const data = await response.json();
-        console.log(data);
-        setTotalViews(data.totalViews);
-      } catch (error) {
-        console.error('获取总浏览量失败:', error);
-      }
+        try {
+            const response = await fetch('/api/views');
+            const data = await response.json();
+            setTotalViews(data.totalViews);
+        } catch (error) {
+            console.error('获取总浏览量失败:', error);
+        }
     }
 
-    fetchTotalViews();
-  }, []);
-
-  return (
-    <div className="text-sm text-gray-500">
-      总浏览量: {totalViews !== null ? totalViews : '加载中...'}
-    </div>
-  );
+    return (
+        <div className="text-sm text-gray-500">
+            总浏览量: {totalViews !== null ? totalViews : '加载中...'}
+        </div>
+    );
 }
