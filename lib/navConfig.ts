562bb2a1877a429c29bdea09b44d690b7db4b860
hfyydd <85127658@qq.com>
2024-08-20 12:22:09 +0800

    商城 -> 市集

--- lib/navConfig.ts
+++ lib/navConfig.ts
@@ -7,7 +7,7 @@ export interface NavItem {
   }
   
   export const navItems: NavItem[] = [
-    { href: '/store', label: '商城', enabled: true },
+    { href: '/store', label: '市集', enabled: true },
     { href: '/education', label: '教育', enabled: true },
     { href: '/dbchat', label: ' 对话数据', enabled: true},
     { href: '/bvideos', label: 'B 站视频', enabled: true },
